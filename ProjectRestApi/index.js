5ddd4f4bf1982a9c686fc347c92b25b81c821ada
Mayur0018 <mayurnish18@gmail.com>
2024-12-20 10:52:34 +0530

    ShortURL

--- ProjectRestApi/index.js
+++ ProjectRestApi/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const user = require("./MOCK_DATA (2).json");
 const app = express();
 const PORT = 8000;
-const fs = require("fs");
 const UserRoutes = require("./Routes/user");
 const { ConectionMongoDb } = require("./connections");
 
@@ -10,20 +8,9 @@ const { ConectionMongoDb } = require("./connections");
 ConectionMongoDb(
   "mongodb+srv://mayurnish18:mayur123@cluster0.ks1hcpc.mongodb.net/"
 );
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended: false }));
-//Routes
-
-app.use("/user", UserRoutes);
-
-// app.use((req,res,next)=>{
-//   console.log("Hello From Middlware 1");
-//   // next();
-//   return res.json({msg : "Hellow From MiddleWare 1"})
-// })
-
-// app.use((req,res,next)=>{
-//   console.log("Hello From Middlware");
-// })
+app.use("/api/users", UserRoutes);
 
 app.listen(PORT, () => console.log(`Serve Was Started Port: ${PORT}`));
+ 
